ad0180df1f53119d1fbe667521e4ebe01a3cfcc9
rin <rhkrfls_@naver.com>
2020-11-02 21:03:03 +0900

    1.singleton 사용 2.UI 적용

---
+++ topshooting/Assets/Scripts/GameLogic/GameScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScene : Singleton<GameScene>
+{
+    private int score;
+
+    [SerializeField]
+    private Text ScoreText;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        score = 0;
+    }
+
+    public void AddScore(int add_Score)
+    {
+        score += add_Score;
+        ScoreText.text = $"Score : {score}"; //'$'를 붙일 시 {} 안에 변수 추가 가능
+    }
+}
